e43985197bbbf4aa08bbb5ccc4a61730c77a8a9d
Thomas Jarrand <thomas.jarrand@gmail.com>
2020-01-19 17:13:59 +0100

    Projects

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Project;
+use Content\ContentManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ProjectController extends AbstractController
+{
+    private $manager;
+
+    public function __construct(ContentManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Route("/creations", name="project", defaults={"_menu"="project"})
+     */
+    public function list()
+    {
+        return $this->render('project/index.html.twig', [
+            'projects' => $this->manager->getContents(Project::class, ['date' => false]),
+        ]);
+    }
+}

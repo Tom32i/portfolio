954c334c805199147b55275d69fe9ee67f521b8d
Thomas Jarrand <thomas.jarrand@elao.com>
2022-01-05 16:53:16 +0100

    Refacto

---
+++ src/Model/Tool.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+class Tool
+{
+    public string $slug;
+    public string $title;
+    public string $description;
+    public string $url;
+    public \DateTimeImmutable $date;
+    public \DateTimeImmutable $lastModified;
+    public int $priority;
+
+    public function getSiteName(): string
+    {
+        return trim(preg_replace('/^https?:\/\/(.+)$/', '$1', $this->url), '/');
+    }
+}
